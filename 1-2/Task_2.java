e3adee8a2a81ee62aea06385b05babd999cdd422
Honoka0914 <tokai.bigbang@gmail.com>
2020-07-30 16:36:52 +0900

    Task_2  課題
    Task_2 課題

---
+++ 1-2/Task_2.java
@@ -0,0 +1,35 @@
+public class Task_2{
+
+public static void main (String[] args){
+
+//①
+ String[] country = {"JAPAN", "AMERICA", "KOREA"};
+ 
+//②
+ System.out.println(country.length);
+ 
+
+//③
+  String[] strArray = new String [3];
+
+  strArray[0] = "りんご";
+  strArray[1] = "もも";
+  strArray[2] = "ぶどう";
+
+//④
+ System.out.println(strArray[1]);
+
+//⑤
+//10,20,30,40,50の要素を初期化して、配列intArrayを作成しています。
+ int[] intArray = { 10, 20, 30, 40, 50 };
+
+//⑥
+//配列intArrayの２番目と５番目の要素を出力しています。
+System.out.println(intArray[1] + intArray[4]);
+
+}
+
+
+
+
+}
