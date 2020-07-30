3165983a4f3ea3e0e2743018ce9073dc841c6690
Honoka0914 <tokai.bigbang@gmail.com>
2020-07-30 15:38:19 +0900

    Task1課題提出
    Task1課題提出

---
+++ 1-1/Task_1.java
@@ -0,0 +1,46 @@
+public class Task_1{
+   public static void main(String[] args){
+//①int型のhogeIntという名前の変数を宣言し、値「50」で初期化
+       
+       int hogeInt = 50;
+
+//②StringクラスのhogeStringという名前の変数を宣言、値「テスト」で初期化
+       
+       String hogeString = "テスト";
+
+//③下記の処理について、何をしているのかコメントを記入してください
+//doulbe型のhogeDoubleという名前の変数を宣言し、値「0.2」で初期化
+double hogeDouble = 0.2;
+
+//④変数 hogeIntを値「100」で上書き
+
+        hogeInt = 100;
+
+//⑤変数 hogeStringの値「合格」で上書き
+
+        hogeString = "合格";
+
+//⑥変数 hogeIntの値を表示
+
+        System.out.println(hogeInt);
+
+//⑦下記の処理について、何をしているのかコメントを記入してください
+//hogeStringの値を表示
+
+        System.out.println(hogeString);
+
+//⑧変数 hogeDouble の値を表示
+
+        System.out.println(hogeDouble);
+
+
+
+
+
+   }
+
+
+
+
+
+}
